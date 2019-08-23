fd0354b6cae11cb07850f9f2c5670059425007d6
Anurag Kondeya <anuragkondeya@Anurags-MacBook-Pro.local>
2019-08-24 05:36:22 +1000

    yargs 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,8 +7,22 @@ yargs.command({
 
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding new note')
+    builder: {
+        title:{
+            desctibe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
-console.log(yargs.argv)
+//console.log(yargs.argv)
+yargs.parse() //parse is required for invoking yargs. In not present, the arguments won't be parsed
