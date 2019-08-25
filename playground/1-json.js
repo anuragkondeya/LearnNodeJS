7df4c2f68996d95394395da2d55ba7b778ac8be6
Anurag Kondeya <anuragkondeya@Anurags-MacBook-Pro.local>
2019-08-25 14:59:32 +1000

    Challenge 18 Storing data with JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,19 +1,29 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
-const book = {
-    title : "Ego is the enemy",
-    author : "Ryan Holiday"
-}
+const fileName = '1-json.json'
 
-const jsonStr = JSON.stringify(book)
-console.log(jsonStr)
+const jsonDataFromFile = fs.readFileSync(fileName)
 
-fs.writeFileSync('1-json.json',jsonStr)
+const jsonDataJson = JSON.parse(jsonDataFromFile);
+jsonDataJson.name = 'Anurag'
+jsonDataJson.age = 35
 
-const json = JSON.parse(jsonStr)
-console.log(json.author)
+fs.writeFileSync(fileName,JSON.stringify(jsonDataJson))
+console.log(chalk.green.inverse('Done'))
 
-const jsonBooks = fs.readFileSync('1-json.json').toString()
-const jsonBooksJson = JSON.parse(jsonBooks)
-console.log(jsonBooksJson.title + ' By ' + jsonBooksJson.author)
+
+
+
+// const jsonStr = JSON.stringify(book)
+// console.log(jsonStr)
+
+// fs.writeFileSync('1-json.json',jsonStr)
+
+// const json = JSON.parse(jsonStr)
+// console.log(json.author)
+
+// const jsonBooks = fs.readFileSync('1-json.json').toString()
+// const jsonBooksJson = JSON.parse(jsonBooks)
+// console.log(jsonBooksJson.title + ' By ' + jsonBooksJson.author)
 
