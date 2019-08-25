31251da02a6c98bb466f533d9991025e4bec081f
Anurag Kondeya <anuragkondeya@Anurags-MacBook-Pro.local>
2019-08-25 15:41:55 +1000

    20 Remove Notes 1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,28 +1,46 @@
+const chalk = require('chalk')
 const yargs = require('yargs')
-const getnotes = require('./notes.js')
+const notes = require('./notes.js')
 
 yargs.version('1.1.0')
 
-yargs.command({
-
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title:{
-            desctibe: 'Note title',
-            demandOption: true,
-            type: 'string'
+yargs.command(
+    {
+        command: 'add',
+        describe: 'Add a new note',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: 'Note body',
+                demandOption: true,
+                type: 'string'
+            }
         },
-        body:{
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
+        handler: function (argv) {
+            notes.addNote(argv.title, argv.body)
         }
-    },
-    handler: function(argv){
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
     }
-})
+)
+
+yargs.command(
+    {
+        command: 'remove',
+        describe: 'remove a note',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: function (argv) {
+            notes.removeNote(argv.title)
+        }
+    }
+)
 //console.log(yargs.argv)
 yargs.parse() //parse is required for invoking yargs. In not present, the arguments won't be parsed
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,53 @@
-const getNotes = function(){
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+const fileName = 'notes.json'
+
+const getNotes = function () {
   return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+  const notes = loadNotes()
+  const duplicates = notes.filter(function (note) {
+    return note.title === title
+  })
+
+  if (duplicates.length === 0) {
+
+    notes.push({
+      title: title,
+      body: body
+    })
+    saveNotes(notes)
+    console.log(chalk.green.inverse('Added'))
+
+  } else {
+    console.log(chalk.red.inverse('Note title taken'))
+  }
+}
+
+const saveNotes = function (notes) {
+  fs.writeFileSync(fileName, JSON.stringify(notes))
+}
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync(fileName)
+    const dataJson = dataBuffer.toString()
+    return JSON.parse(dataJson)
+  } catch (e) {
+    return []
+  }
+}
+
+const removeNote = function (title) {
+ console.log('Note removed')
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote
+}
