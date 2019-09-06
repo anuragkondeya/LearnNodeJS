f112a8ad1fcea3609b60aeb7651c540c3a6ed503
Anurag Kondeya <anuragkondeya@Anurags-MacBook-Pro.local>
2019-09-07 03:24:59 +1000

    ADD: seach fields and bar

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,27 @@
-console.log('clinet side java script is loaded')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+const fetchWeatherData = (url) => {
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+            } else {
+                message1.textContent = data.forecast
+                message2.textContent = data.location
+            }
+        })
+    })
+}
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    message1.textContent = "Searching..."
+    message2.textContent = ""
+    const location = search.value
+    const url = "http://localhost:3000/weather?address=" + location
+    fetchWeatherData(url)
+
+})
